07fe0db01bb4c6323d97a8245bfde272f159d19e
Tony Migeon <tonymigeonpro@gmail.com>
2024-01-23 20:34:51 +0100

    Refactored, now has callable function for items and monsters

---
+++ ScrapyProject/Scripts/split_file.py
@@ -0,0 +1,42 @@
+import json
+from math import e
+import os
+
+
+project_dir = os.path.abspath(os.getcwd())
+static_data_dir_path = os.path.join(project_dir, '..', 'StaticData')
+subset_data_dir_path = os.path.join(project_dir, '..', 'SubsetDataForTests')
+items_data_file_path = os.path.join(static_data_dir_path, 'items.json')
+monsters_data_file_path = os.path.join(project_dir, '..', 'ScrappedData', 'ScrappedFiles', 'ScrappedMonsters', 'en_monsters_stats_data.json')
+
+def get_subset_items_data(amountWanted):
+    with open(items_data_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    subset_data = data[amountWanted]
+    try:
+        with open('subset_items.json', 'w', encoding='utf-8') as new_file:
+            json.dump(subset_data, new_file, ensure_ascii=False, indent=2)
+        print("Subset of items.json written to /SubsetDataForTests/subset_items.json.")
+    except Exception as e:
+        print("Failed to get subset of items : ", str(e))
+        return False
+    finally:
+        return True
+    
+
+def get_subset_monsters_data(amountWanted):
+    with open(monsters_data_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    subset_data = data[amountWanted]
+    try:
+        with open('subset_monsters.json', 'w', encoding='utf-8') as new_file:
+            json.dump(subset_data, new_file, ensure_ascii=False, indent=2)
+        print("Subset of items.json written to /SubsetDataForTests/subset_monsters.json.")
+    except Exception as e:
+        print("Failed to get subset of monsters : ", str(e))
+        return False
+    finally:
+        return True
+
